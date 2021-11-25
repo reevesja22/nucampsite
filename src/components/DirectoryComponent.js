1f99f725646ad046eb622449c771c9afd82c9440
JakeReeves <reevesja01@gmail.com>
2021-11-25 08:57:15 -0600

    Controlled Forms

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
     return (
         <Card>
+            <Link to={`/directory/${campsite.id}`}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
             </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -24,6 +27,16 @@ function Directory(props) {
 
     return (
         <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 {directory}
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FootComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -33,7 +34,16 @@ class Main extends Component {
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        };
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                />
+            )
+        } 
 
         return (
             <div>
@@ -42,6 +52,8 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/aboutus' component={() => <About partners={this.state.partners} />}  />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
