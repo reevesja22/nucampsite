6bfcb9ce1ca7f90627e8aeebeefedf26c08b84a0
JakeReeves <reevesja01@gmail.com>
2021-11-28 20:40:02 -0600

    Combining Reducers

---
+++ src/redux/campsites.js
@@ -0,0 +1,9 @@
+import { CAMPSITES } from '../shared/campsites';
+
+export const Campsites = (state = CAMPSITES, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
+
