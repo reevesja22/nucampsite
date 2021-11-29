874e38d0e6a92b4eefdfbd9c79419238b9c1b879
JakeReeves <reevesja01@gmail.com>
2021-11-28 20:56:43 -0600

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -45,8 +45,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -118,14 +118,14 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return(
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
                 <p>{comments.map(comment => <div key={comment.id}><p>{comment.text}</p><p> -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}  </p> </div>)} 
                 </p>
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
         } else {
@@ -151,7 +151,11 @@ function CampsiteInfo(props) {
              </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id} 
+                    />
                 </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -17,6 +18,10 @@ const mapStateToProps = state => {
         promotions: state.promotions
     }
 }
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
 
 class Main extends Component {
 
@@ -37,7 +42,8 @@ class Main extends Component {
             return (
                 <CampsiteInfo 
                 campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                addComment={this.props.addComment} 
                 />
             )
         } 
@@ -59,4 +65,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
