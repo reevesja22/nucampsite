4198822170771fcbaf5bcd08fe97f0131ac4b075
JakeReeves <reevesja01@gmail.com>
2021-12-03 18:41:00 -0600

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -48,7 +48,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -120,14 +120,14 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments) {
         return(
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
                 <p>{comments.map(comment => <div key={comment.id}><p>{comment.text}</p><p> -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}  </p> </div>)} 
                 </p>
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
         } else {
@@ -176,7 +176,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id} 
                     />
                 </div>
